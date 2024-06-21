57dde9b720b8bc59a6afdd323f75f88f06e460bb
VaishnaviNEU <145122714+VaishnaviNEU@users.noreply.github.com>
2024-06-21 09:35:40 -0400

    test(Jenkins): Terraform Files.

--- KMS_Keys.tf
+++ KMS_Keys.tf
@@ -1,11 +1,11 @@
-resource "aws_kms_key" "eks_secrets_encryption" {
-  description             = var.eks_secrets_encryption_description
-  deletion_window_in_days = var.eks_secrets_encryption_deletion_window_in_days
-
-  tags = {
-    Name = "${var.cluster_name}-eks-secrets-encryption"
-  }
-}
+              resource "aws_kms_key" "eks_secrets_encryption" {
+                description             = var.eks_secrets_encryption_description
+                deletion_window_in_days = var.eks_secrets_encryption_deletion_window_in_days
+              
+                tags = {
+                  Name = "${var.cluster_name}-eks-secrets-encryption"
+                }
+              }
 
 resource "aws_kms_alias" "eks_secrets_encryption_alias" {
   name          = "alias/EKS-Key"
