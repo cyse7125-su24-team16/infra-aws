58c433597eca785fabe67e8c5ee1a5669d17841b
VaishnaviNEU <145122714+VaishnaviNEU@users.noreply.github.com>
2024-06-21 10:53:37 -0400

    fix(Jenkins): Updated for Push and Pull Steps

--- EKS_Cluster.tf
+++ EKS_Cluster.tf
@@ -1,109 +1,124 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+variable "aws_region" {
+  type = string
+}
+variable "vpc_cidr" {
+  type = string
+}
+variable "vpc_name" {
+  type = string
+}
+variable "us-east-1b_availability_zone" {
+  type = string
+}
+variable "us-east-1c_availability_zone" {
+  type = string
+}
+variable "us-east-1d_availability_zone" {
+  type = string
+}
+variable "public_subnet_1_cidr" {
+  type = string
+}
+variable "public_subnet_2_cidr" {
+  type = string
+}
+variable "public_subnet_3_cidr" {
+  type = string
+}
+variable "private_subnet_1_cidr" {
+  type = string
+}
+variable "private_subnet_2_cidr" {
+  type = string
+}
+variable "private_subnet_3_cidr" {
+  type = string
+}
+variable "public_subnet_1_name" {
+  type = string
+}
+variable "public_subnet_2_name" {
+  type = string
+}
+variable "public_subnet_3_name" {
+  type = string
+}
+variable "private_subnet_1_name" {
+  type = string
+}
+variable "private_subnet_2_name" {
+  type = string
+}
+variable "private_subnet_3_name" {
+  type = string
+}
 
-  # EKS Cluster Configuration
-  cluster_name        = var.cluster_name
-  cluster_version     = var.cluster_version
-  authentication_mode = var.eks_authentication_mode
-  cluster_ip_family   = var.eks_ip_family
+variable "kms_key_deletion_window" {
+  type = number
+}
+variable "kms_key_rotation_period" {
+  type = number
+}
+variable "eks_secrets_key_alias" {
+  type = string
+}
+variable "ebs_volumes_key_alias" {
+  type = string
+}
 
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
+variable "eks_cluster_name" {
+  type = string
+}
+variable "eks_cluster_version" {
+  type = string
+}
+variable "eks_cluster_authentication_mode" {
+  type = string
+}
+variable "eks_cluster_ip_family" {
+  type = string
+}
+variable "eks_cluster_service_role" {
+  type = string
+}
+variable "eks_cluster_node_group_name" {
+  type = string
+}
+variable "node_group_instance_capacity_type" {
+  type = string
+}
 
-  # Observability
-  cluster_enabled_log_types = [
-    "api",
-    "audit",
-    "authenticator",
-    "controllerManager",
-    "scheduler"
-  ]
+variable "node_group_min_size" {
+  type        = number
+  description = "Minimum size of the node group"
+}
 
-  # Encryption
-  cluster_encryption_config = {
-    resources        = ["secrets"]
-    provider_key_arn = aws_kms_key.eks_secrets_encryption.arn
-  }
+variable "node_group_max_size" {
+  type        = number
+  description = "Maximum size of the node group"
+}
 
-  # Identity and Access
-  iam_role_arn = aws_iam_role.Eks_Cluster_Role.arn
+variable "node_group_desired_size" {
+  type        = number
+  description = "Desired size of the node group"
+}
 
-  # Addons
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-    eks-pod-identity-agent = {
-      most_recent = true
-    }
-    aws-ebs-csi-driver = {
-      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
-      most_recent              = true
-    }
-  }
+variable "node_group_instance_types" {
+  type        = list(string)
+  description = "Instance types for the node group"
+}
 
-  # Networking
-  vpc_id                    = aws_vpc.VPC.id
-  subnet_ids                = concat(aws_subnet.Public_Subnets[*].id, aws_subnet.Private_subnets[*].id)
-  control_plane_subnet_ids  = aws_subnet.Private_subnets[*].id
-  cluster_security_group_id = aws_security_group.EKS_Security_Group.id
+variable "node_group_instance_ami_type" {
+  type        = string
+  description = "AMI type for the node group instances"
+}
 
-  # Tags
-  tags = {
-    Name = var.cluster_name
-  }
+variable "node_group_instance_disk_size" {
+  type        = number
+  description = "Disk size (in GB) for the node group instances"
+}
 
-  # Explicit Dependencies for EKS Cluster
-  depends_on = [
-    aws_subnet.Public_Subnets,
-    aws_security_group.EKS_Security_Group,
-    aws_kms_key.eks_secrets_encryption
-  ]
-
-  eks_managed_node_groups = {
-    eks_node_group = {
-      name = var.node_group_name
-
-      # Scaling Configuration  
-      min_size     = var.min_size
-      max_size     = var.max_size
-      desired_size = var.desired_size
-
-      # Node Configuration
-      instance_types = var.instance_types
-      ami_type       = var.ami_type
-      capacity_type  = var.node_group_capacity_type
-      disk_size      = var.disk_size
-
-      # Node IAM Role
-      iam_role_arn = aws_iam_role.Eks_Worker_Node_Role.arn
-
-      # Node Group Networking
-      subnet_ids = aws_subnet.Private_subnets[*].id
-
-      # Optional: Set maximum unavailable nodes during updates
-      update_config = {
-        max_unavailable = var.max_unavailable
-      }
-
-      # Node Group Tags
-      tags = {
-        Name = var.node_group_name
-      }
-
-      # Explicit Dependencies for Node Group(s)
-      depends_on = [
-        aws_iam_role.Eks_Worker_Node_Role.arn,
-      ]
-    }
-  }
-  # Cluster access entry to add the current caller identity as an administrator
-  enable_cluster_creator_admin_permissions = true
+variable "node_group_instance_max_unavailable" {
+  type        = number
+  description = "Maximum number of unavailable nodes during updates"
 }
