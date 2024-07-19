3e378a1d69f04e33050d57655c7396d39134d447
VaishnaviNEU <145122714+VaishnaviNEU@users.noreply.github.com>
2024-07-19 00:14:34 -0400

    feat: add iam for autoscaler configuration file

---
+++ Metric-Server.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "metrics_server" {
+  name       = var.Metrics_server_chart_name
+  namespace  = var.Autoscaler_namespace
+  repository = var.bitnami_repository_url
+  chart      = var.Metrics_Chart_Name
+  version    = var.Metrics_server_chart_version
+
+  values = [file(var.Metrics_Yaml_File)]
+
+  depends_on = [
+    module.eks,
+  ]
+}
