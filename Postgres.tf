50e9bcb0cf237791754bc78fda55b95115c00226
VaishnaviNEU <145122714+VaishnaviNEU@users.noreply.github.com>
2024-07-19 00:35:14 -0400

    chore: rename and update Postgres.tf to Postgres.tf

--- Postgres.tf
+++ Postgres.tf
@@ -5,10 +5,9 @@ resource "helm_release" "postgresql" {
   chart      = var.postgresql_chart_name
   version    = var.postgresql_chart_version
 
-  values = [file("values.yaml")]
+  values = [file(var.Postgresql_Yaml_File)]
 
   depends_on = [
     module.eks,
   ]
 }
-
